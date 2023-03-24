8e5452f57a1a035e2d86b65662749f47a4dc6a89
KARTIKI BHANDAKKAR <52490636+kartiki-b31@users.noreply.github.com>
2023-03-23 21:12:07 -0400

    Time: 0 ms (100.00%), Space: 40.2 MB (11.18%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp=new int[n+3];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        
+        if(n<3){
+            return dp[n];
+            
+        }
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+        
+        
+    }
+}
