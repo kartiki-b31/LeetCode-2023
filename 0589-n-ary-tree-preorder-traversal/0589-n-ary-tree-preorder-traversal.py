71b7de2fce0a5902aec3c3cfe759c68ba1c58143
Kartiki Bhandakkar <52490636+kartiki-b31@users.noreply.github.com>
2023-05-22 14:00:25 -0400

    Time: 79 ms (5.45%), Space: 18.5 MB (14.17%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def __init__(self):
+        self.arr=[]
+    def preorder(self, root: 'Node') -> List[int]:
+        if root==None:
+            return None
+        self.arr.append(root.val)
+        for i in range(len(root.children)):
+            self.preorder(root.children[i])
+            
+        return self.arr
+        
