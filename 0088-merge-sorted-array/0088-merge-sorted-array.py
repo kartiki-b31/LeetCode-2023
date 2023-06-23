6573f0d741eb48c8ed654c7db1f4ec899e3a68e1
Kartiki Bhandakkar <52490636+kartiki-b31@users.noreply.github.com>
2023-06-23 15:16:20 -0400

    Time: 56 ms (37.54%), Space: 16.4 MB (15.48%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -4,6 +4,7 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         for i in range(n):
+            #nums1.append(nums2[i])
             nums1[i+m]=nums2[i]
             
             
