5ef2f63a426630f1a880e381c6e8b4d9aafbcc14
Kartiki Bhandakkar <52490636+kartiki-b31@users.noreply.github.com>
2023-07-16 01:48:35 -0400

    Time: 655 ms (93.83%), Space: 30.5 MB (77.66%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,15 +1,6 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         
-#         ans=[]
-        
-#     #Ratto_automatic
-#         for i in range(len(nums)):
-#             for j in range(i+1,len(nums)+1):
-#                 ans.append(sum(nums[i:j]))
-
-               
-#         return max(ans)
         maxi=float("-infinity")
         sums=0
         for i in range(len(nums)):
