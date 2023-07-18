3380b32a78e8296ee9ac3c76e8d2e7b4bb2f3483
Kartiki Bhandakkar <52490636+kartiki-b31@users.noreply.github.com>
2023-07-18 14:59:47 -0400

    Time: 53 ms (38.61%), Space: 16.4 MB (36.63%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,31 +1,50 @@
 class Solution:
+    def swap(self, arr,i,j):
+        temp=arr[i]
+        arr[i]=arr[j]
+        arr[j]=temp
+        
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        cnt0=0
-        cnt1=0
-        cnt2=0
-        arr=[]
+#         cnt0=0
+#         cnt1=0
+#         cnt2=0
+#         arr=[]
         
-        for i in range(len(nums)):
-            if nums[i]==0:
-                cnt0=cnt0+1
-            elif nums[i]==1:
-                cnt1=cnt1+1
-            else:
-                cnt2=cnt2+1
+#         for i in range(len(nums)):
+#             if nums[i]==0:
+#                 cnt0=cnt0+1
+#             elif nums[i]==1:
+#                 cnt1=cnt1+1
+#             else:
+#                 cnt2=cnt2+1
                 
-        for i in range(cnt0):
-            nums[i]=0
+#         for i in range(cnt0):
+#             nums[i]=0
         
-        for i in range(cnt0,cnt0+cnt1):
-            nums[i]=1
+#         for i in range(cnt0,cnt0+cnt1):
+#             nums[i]=1
             
-        for i in range(cnt0+cnt1,len(nums)):
-            nums[i]=2
+#         for i in range(cnt0+cnt1,len(nums)):
+#             nums[i]=2
             
         
-        return nums
+#         return nums
+
+        low=0
+        mid=0
+        high=len(nums)-1
+        while(mid<=high):
+            if nums[mid]==0:
+                self.swap(nums,low,mid)
+                low=low+1
+                mid=mid+1
+            elif nums[mid]==1:
+                mid=mid+1
             
+            else:
+                self.swap(nums,mid,high)
+                high=high-1
         
