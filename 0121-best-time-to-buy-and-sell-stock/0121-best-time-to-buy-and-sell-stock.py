d5d823b4d4e89de4eb4d4910cc3a95ab377557ac
Kartiki Bhandakkar <52490636+kartiki-b31@users.noreply.github.com>
2023-07-18 15:13:16 -0400

    Time: 933 ms (85.96%), Space: 27.5 MB (23.65%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,9 +3,9 @@ class Solution:
         maxPro=0
         minPrice=float("infinity")
         for i in range(len(prices)):
-            if minPrice> prices[i]:
+            if minPrice > prices[i]:
                 minPrice=prices[i]
-                
-            maxPro=max(maxPro,prices[i]-minPrice)
+            if maxPro < (prices[i]-minPrice):    
+                maxPro=prices[i]-minPrice
             
         return maxPro
