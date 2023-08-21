982e951536bd3c0e51ede6e21eb5769a7077d857
Kartiki Bhandakkar <52490636+kartiki-b31@users.noreply.github.com>
2023-08-21 19:40:13 -0400

    Time: 54 ms (88.05%), Space: 16.6 MB (24.02%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -2,10 +2,22 @@ import math
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
         
-        for i in range(k):
-            max_index=gifts.index(max(gifts))
-            gifts[max_index]=math.floor(math.sqrt(gifts[max_index]))
+#         for i in range(k):
+#             max_index=gifts.index(max(gifts))
+#             gifts[max_index]=math.floor(math.sqrt(gifts[max_index]))
             
             
-        return int(sum(gifts))
+#         return int(sum(gifts))
+        gifts=[-1*i for i in gifts]
+        heapq.heapify(gifts)
+        #print(gifts)
         
+        for i in range(k):
+            top_element=heapq.heappop(gifts)
+            #print(gifts)
+            sqrt_top_element=math.floor(math.sqrt(-1*top_element))
+            heapq.heappush(gifts, -1*sqrt_top_element)
+            
+        
+        return -1*int(sum(gifts))
+            
