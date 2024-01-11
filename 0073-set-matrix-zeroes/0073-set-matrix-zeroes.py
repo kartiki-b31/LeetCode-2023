3ab46fcc312bd016c80358c42bd235956972cb80
Kartiki Bhandakkar <52490636+kartiki-b31@users.noreply.github.com>
2024-01-11 18:08:02 -0500

    Time: 107 ms (79.43%), Space: 18.2 MB (35.98%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,19 +3,39 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        #col[m]=matrix[0][..]
+        #row[n]=matrix[..][0]
         n=len(matrix)
         m=len(matrix[0])
-        row=[0]*n
-        col=[0]*m
+        col0=1
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j]==0:
+                    matrix[i][0]=0
+                    if j!=0:
+                        matrix[0][j]=0
+                    else:
+                        col0=0
+        
+        for i in range(1,n):
+            for j in range(1,m):
+                if matrix[i][j]!=0:
+                    if matrix[0][j]==0 or matrix[i][0]==0:
+                        matrix[i][j]=0
+                        
+        if matrix[0][0]==0:
+            for j in range(m):
+                matrix[0][j]=0
+        if col0==0:
+            for i in range(n):
+                matrix[i][0]=0
+                
+                
+        return matrix
+    
+    
+    
 
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
-                if matrix[r][c]==0:
-                    row[r]=1
-                    col[c]=1
 
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
-                if row[r] or col[c]:
-                    matrix[r][c]=0
+
         
