566f8844dd7a42840c843b75ac84f1574da06f4f
Kartiki Bhandakkar <52490636+kartiki-b31@users.noreply.github.com>
2024-09-21 17:43:29 -0400

    Time: 51 ms (5.3%), Space: 17.7 MB (38.6%) - LeetHub

---
+++ 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def boundaryOfBinaryTree(self, root: Optional[TreeNode]) -> List[int]:
+        def leftdfs(node):
+            if node:
+                if node.left is None and node.right is None:
+                    return []
+                else:
+                    if node.left:
+                        return [node.val]+leftdfs(node.left)
+                    else:
+                        return [node.val]+leftdfs(node.right)
+            else:
+                return []
+        
+        def findLeaves(node):
+            if node:
+                if node.right is None and node.left is None:
+                    return [node.val]
+                else:
+                    return findLeaves(node.left)+findLeaves(node.right)
+            else:
+                return []
+        
+
+        def rightdfs(node):
+            if node:
+                if node.left is None and node.right is None:
+                    return []
+                else:
+                    if node.right:
+                        return rightdfs(node.right)+[node.val]
+                    else:
+                        return rightdfs(node.left)+[node.val]
+            else:
+                return []
+        
+
+        leftandright=[root.val]+leftdfs(root.left)+findLeaves(root.left)+findLeaves(root.right)+rightdfs(root.right)
+        return leftandright
