d915c0805fb6c240e3a449268912852bbdad51fa
Kartiki Bhandakkar <52490636+kartiki-b31@users.noreply.github.com>
2024-09-23 05:20:23 -0400

    Time: 841 ms (20.33%), Space: 21.3 MB (54.04%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.py
+++ 0952-word-subsets/0952-word-subsets.py
@@ -2,6 +2,7 @@ class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
         res=[]
         tempDict=Counter()
+        #bitwiseOR, takes the union of both the counters
         for w in words2:
             tempDict|=Counter(w)
         
