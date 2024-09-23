ce161527dbb0c8d711b688b193f6bb2d5d2a8eda
Kartiki Bhandakkar <52490636+kartiki-b31@users.noreply.github.com>
2024-09-23 05:13:43 -0400

    Time: 865 ms (13.64%), Space: 20.5 MB (99.16%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.py
+++ 0952-word-subsets/0952-word-subsets.py
@@ -1,21 +1,13 @@
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
-        flag_arr=[False]*len(words1)
-        #print(flag_arr)
-        for word in range(len(words1)):
-            flag_arr[word]=False
-            for letter in words2:
-                if letter not in words1[word]:
-                    flag_arr[word]=False
-                    break
-                flag_arr[word]=True
-                
-
-        #[f, True, True, True, True]
-        #print(flag_arr)
         res=[]
-        for i in range(len(flag_arr)):
-            if flag_arr[i]==True:
-                res.append(words1[i])
-        #print(res)
+        tempDict=Counter()
+        for w in words2:
+            tempDict|=Counter(w)
+        
+        for w in words1:
+            if not tempDict-Counter(w):
+                res.append(w)
+        print(tempDict)
+
         return res
