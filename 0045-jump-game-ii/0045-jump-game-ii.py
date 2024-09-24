09d629f8ea330bfbaa5ab26354c251a8e09ba93c
Kartiki Bhandakkar <52490636+kartiki-b31@users.noreply.github.com>
2024-09-24 01:46:51 -0400

    Time: 108 ms (49.29%), Space: 17.6 MB (78.15%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,23 +1,19 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        totalJumps=0
+        totaljump=0
         destination=len(nums)-1
         coverage=0
-        lastJumpidx=0
-        if(len(nums)==1):
+        lastjumpidx=0
+        if len(nums)==1:
             return 0
         
         for i in range(len(nums)):
             coverage=max(coverage,i+nums[i])
-            if i==lastJumpidx:
-                lastJumpidx=coverage
-                totalJumps+=1
-                
+            if i==lastjumpidx:
+                lastjumpidx=coverage
+                totaljump+=1
+
                 if coverage>=destination:
-                    return totalJumps
-                
-                
+                    return totaljump
         
-        
-        return totalJumps
-        
+        return totaljump
