32602c0a9d60302dc6e6c3189891fa6fcdfc38ee
Kartiki Bhandakkar <52490636+kartiki-b31@users.noreply.github.com>
2024-09-24 02:31:58 -0400

    Time: 37 ms (44.97%), Space: 16.6 MB (52.38%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,20 +1,22 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack=[]
-        
-        for i in s:
+        d={"{":"}","(":")","[":"]"}
+        count1=0
+        count2=0
+        count3=0
+        stack=[] #()
+        for ch in s:
+            if ch=="{" or ch=="(" or ch=="[":
+                stack.append(ch)
+            elif ch==")" and (len(stack)==0 or stack.pop()!='('):#0 and (0 or 0)
+                return False
+            elif ch=="}" and (len(stack)==0 or stack.pop()!='{'):#0 and (0 or 0)
+                return False
+            elif ch=="]" and (len(stack)==0 or stack.pop()!='['):#0 and (0 or 0)
+                return False
             
-            if i=='(' or i=='{' or i=='[':
-                stack.append(i)
-            elif i==')' and (len(stack)==0 or stack.pop()!='('):
-                return False
-            elif i=='}' and (len(stack)==0 or stack.pop()!='{'):
-                return False
-            elif i==']' and (len(stack)==0 or stack.pop()!='['):
-                return False 
-        
         if len(stack)!=0:
             return False
         else:
             return True
-        
+                
