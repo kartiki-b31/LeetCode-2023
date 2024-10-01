2ccc8d2775447d3dd903c89a46f4057216d53d47
Kartiki Bhandakkar <52490636+kartiki-b31@users.noreply.github.com>
2024-09-30 21:55:28 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -9,12 +9,16 @@ class Solution:
         s=s.replace("@","")
         s=s.replace("#","")
         s=s.replace("_","")
+        for char in s:
+            if char.isalnum():
+                new_str=new_str+char
+
         print(s)
         i=0
-        n=len(s)-1
+        n=len(new_str)-1
         j=n
         while i<=j:
-            if s[i]!=s[j]:
+            if new_str[i]!=new_str[j]:
                 return False
             
             i+=1
