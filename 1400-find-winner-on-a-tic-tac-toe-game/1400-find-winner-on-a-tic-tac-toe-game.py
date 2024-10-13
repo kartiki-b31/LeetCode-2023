f902ef13938fb39c86c668b38dbdf5326f99cb89
Kartiki Bhandakkar <52490636+kartiki-b31@users.noreply.github.com>
2024-10-12 21:54:17 -0400

    Time: 40 ms (36.44%), Space: 16.6 MB (30.12%) - LeetHub

--- 1400-find-winner-on-a-tic-tac-toe-game/1400-find-winner-on-a-tic-tac-toe-game.py
+++ 1400-find-winner-on-a-tic-tac-toe-game/1400-find-winner-on-a-tic-tac-toe-game.py
@@ -1,115 +1,32 @@
 class Solution:
     def tictactoe(self, moves: List[List[int]]) -> str:
-        grid = [[0] * 3 for _ in range(3)] 
-        
+        grid=[[""]*3 for i in range(3)]
         for i in range(len(moves)):
-            if i%2==0: #i=2
-                grid[moves[i][0]][moves[i][1]]="X" #[0,1]
+            if i%2==0:
+                grid[moves[i][0]][moves[i][1]]="A"
             elif i%2!=0:
-                grid[moves[i][0]][moves[i][1]]="O"
+                grid[moves[i][0]][moves[i][1]]="B"
         
         print(grid)
-        
-        def checkcol(i):
-            j=0
-            if grid[j][i]=="X":
-                while j<3:
-                    if grid[j][i]!="X":
-                        break
-                    j+=1
-                if j==3:
-                    return True,"X"
-                else:
-                    return False,"0"
-            elif grid[j][i]=="O":
-                while j<3:
-                    if grid[j][i]!="O":
-                        break
-                    j+=1
-                if j==3:
-                    return True,"O"
-                else:
-                    return False,"0"
-            else:
-                return False,"0"
 
-        def checkrow(i):
-            j=0
-            if grid[i][j]=="X":
-                while j<3:
-                    if grid[i][j]!="X":
-                        break
-                    j+=1
-                if j==3:
-                    return True,"X"
-                else:
-                    return False,"0"
-            elif grid[i][j]=="O":
-                while j<3:
-                    if grid[i][j]!="O":
-                        break
-                    j+=1
-                if j==3:
-                    return True,"O"
-                else:
-                    return False,"0"
-            else:
-                return False,"0"
-        def checkleftdiag(sym):
-            if grid[0][0]==grid[1][1] and grid[1][1]==grid[2][2]:
-                return True
-            else:
-                return False
-        def checkrightdiag(sym):
-            if grid[2][0]==grid[1][1] and grid[1][1]==grid[0][2]:
-                return True
-            else:
-                return False
-
-        #print(checkcol(0))
-        #print(checkrow(0))
-        if grid[0][0]=="X":
-            if checkleftdiag("X"):
-                print("me")
-                return "A"
-        
-
-        if grid[0][0]=="O":
-            if checkleftdiag("O"):
-                print("try")
-                return "B"
-
+        if grid[0][0]==grid[0][1]==grid[0][2] and grid[0][0]!="":
+            return grid[0][0]
+        if grid[1][0]==grid[1][1]==grid[1][2] and grid[1][0]!="":
+            return grid[1][0]
+        if grid[2][0]==grid[2][1]==grid[2][2] and grid[2][0]!="":
+            return grid[2][0]
+        if grid[0][0]==grid[1][0]==grid[2][0] and grid[0][0]!="":
+            return grid[0][0]
+        if grid[0][1]==grid[1][1]==grid[2][1] and grid[0][1]!="":
+            return grid[0][1]
+        if grid[0][2]==grid[1][2]==grid[2][2] and grid[0][2]!="":
+            return grid[0][2]
+        if grid[0][0]==grid[1][1]==grid[2][2] and grid[0][0]!="":
+            return grid[0][0]
+        if grid[0][2]==grid[1][1]==grid[2][0] and grid[0][2]!="":
+            return grid[0][2]
             
-        if grid[0][2]=="X":
-            if checkrightdiag("X"):
-                print("inside")
-                return "A"
-           
 
-        if grid[0][2]=="O":
-            if checkrightdiag("O"):
-                print("iam")
-                return "B"
-        
-        for i in range(3):
-            anscol,vari=checkcol(i)
-            if anscol:
-                if vari=="X":
-                    print("onit")
-                    return "A"
-                elif vari=="O":
-                    return "B"
-        
-        for i in range(3):
-            ansrow,vari=checkrow(i)
-            if ansrow:
-                if vari=="X":
-                    print("onit")
-                    return "A"
-                elif vari=="O":
-                    return "B"
-
-        len_m=len(moves)
         if len(moves)==9:
             return "Draw"
         else:
