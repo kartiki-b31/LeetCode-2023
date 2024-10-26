a1002a5d626873ce7ed34ff190dd9c943cdf5e79
Kartiki Bhandakkar <52490636+kartiki-b31@users.noreply.github.com>
2024-10-26 18:18:46 -0400

    Time: 5 ms (44.87%), Space: 17.8 MB (63.69%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,15 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        c=Counter(nums)
-        #print(c)
-        n=len(nums)
-        #print(n)
-        # sum=0
-        # sum2=0
-        # sum2=n*(n-1)
-        for i in range(n+1):
-            if i not in c:
-                return i   
-            
-            
-        
+        xorval=len(nums)
+        for i in range(len(nums)):
+            xorval^=i^nums[i]
+
+        return xorval
