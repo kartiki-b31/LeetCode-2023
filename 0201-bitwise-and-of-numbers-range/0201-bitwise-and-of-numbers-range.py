55b880b997d58c254b53198455e68185fa86a5f2
Kartiki Bhandakkar <52490636+kartiki-b31@users.noreply.github.com>
2024-10-26 18:34:11 -0400

    Time: 11 ms (22.09%), Space: 16.7 MB (6.04%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
@@ -3,6 +3,7 @@ class Solution:
         '''
         [5,6,7]
         5&6&7=(5)
+        https://www.youtube.com/watch?v=gLZZyvXTRLk
         '''
         while right>left:
             right=right & (right-1)
