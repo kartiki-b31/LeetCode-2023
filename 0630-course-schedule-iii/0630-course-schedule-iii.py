8f187fcde1d50d60bf492ff05636a9208667e44f
Kartiki Bhandakkar <52490636+kartiki-b31@users.noreply.github.com>
2024-11-15 06:22:54 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0630-course-schedule-iii/0630-course-schedule-iii.py
@@ -0,0 +1,25 @@
+#[[100, 200], [1000, 1250], [200, 1300], [2000, 3200]]
+
+class Solution:
+    def scheduleCourse(self, courses: List[List[int]]) -> int:
+
+        courses.sort(key=lambda x:x[1])
+        print(courses)
+
+        maxHeap=[]
+        nums=[100,1000,200,2000]
+        
+        t=0
+        for i in range(len(courses)):
+            t+=courses[i][0]
+            if t<=courses[i][1]:
+                heappush(maxHeap,-courses[i][0])
+                heapq.heapify(maxHeap)
+            else:
+                break
+        print(t)
+        print(maxHeap)
+        
+
+        return len(maxHeap)
+        
