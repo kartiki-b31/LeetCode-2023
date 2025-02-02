71128ca4690b9dd8e90451c2bc9af00a16713ce4
Kartiki Bhandakkar <52490636+kartiki-b31@users.noreply.github.com>
2025-02-02 15:14:58 -0500

    Time: 0 ms (100%), Space: 17.8 MB (73.12%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,11 +1,49 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans=[]
 
+        def recursive(curr_subset,idx):
+
+            if idx>=len(nums):
+                ans.append(curr_subset)
+                return
+            #no pick # not picking current indiex and movieng to th next
+            recursive(curr_subset,idx+1)
+            #yes pick
+            recursive(curr_subset+[nums[idx]],idx+1)
+
+
+
+        curr_subset=[]
+        recursive(curr_subset,0)
+        return ans
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        '''
         def recursive(subset,i):
             if len(nums)==i:
                 ans.append(subset[:])
                 return 
-                
+
             subset.append(nums[i])
             recursive(subset,i+1)
             subset.pop()
@@ -17,3 +55,4 @@ class Solution:
         ans=[]
         recursive(subset,0)
         return ans
+        '''
