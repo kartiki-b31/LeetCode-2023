c291eb67c94ab9d71d03e043ed4e5b3caf1513be
Kartiki Bhandakkar <52490636+kartiki-b31@users.noreply.github.com>
2025-02-16 01:07:19 -0500

    Time: 0 ms (100%), Space: 17.6 MB (86.49%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,21 +1,16 @@
-import math
 class Solution:
     def isHappy(self, n: int) -> bool:
+        def num_sq(str_n):
 
-        def get_next(n):
-            total_sum = 0
-            while n > 0:
-                n, digit = divmod(n, 10)
-                total_sum += digit ** 2
-            return total_sum
-
-        seen = set()
-        while n != 1 and n not in seen:
-            seen.add(n)
-            n = get_next(n)
-
-        return n == 1
-            
-            
+            val=0
+            for dig in str(str_n):
+                squared=(int(dig))**2
+                val+=squared
         
-        
+            return val
+
+        if n==1:
+            return True
+        if n==4:
+            return False
+        return self.isHappy(num_sq(n))
